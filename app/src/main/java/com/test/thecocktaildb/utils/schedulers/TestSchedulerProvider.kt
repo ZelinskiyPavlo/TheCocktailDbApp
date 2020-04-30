1379697bf50310a007bb0814101e8d00b0e99d6a
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2020-04-30 17:20:51 +0300

    Added Scheduler provider to make possible using different RxJava Schedulers in prod and test code

---
+++ app/src/main/java/com/test/thecocktaildb/utils/schedulers/TestSchedulerProvider.kt
@@ -0,0 +1,16 @@
+package com.test.thecocktaildb.utils.schedulers
+
+import io.reactivex.Scheduler
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
+
+class TestSchedulerProvider @Inject constructor() : SchedulerProvider {
+
+    override fun io(): Scheduler = Schedulers.trampoline()
+
+    override fun ui(): Scheduler = Schedulers.trampoline()
+
+    override fun trampoline(): Scheduler = Schedulers.trampoline()
+
+    override fun computation(): Scheduler = Schedulers.trampoline()
+}
