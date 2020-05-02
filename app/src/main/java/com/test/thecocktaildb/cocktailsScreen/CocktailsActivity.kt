2325809dca9f0bd20da54a6286e3f0402a83d900
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2020-05-02 11:31:28 +0300

    Added activity configuration in CocktailsActivity

--- app/src/main/java/com/test/thecocktaildb/cocktailsScreen/CocktailsActivity.kt
+++ app/src/main/java/com/test/thecocktaildb/cocktailsScreen/CocktailsActivity.kt
@@ -2,12 +2,31 @@ package com.test.thecocktaildb.cocktailsScreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.test.thecocktaildb.R
 
 class CocktailsActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolBar))
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+//  TODO: Check if this properly works
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment)
+            .navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
