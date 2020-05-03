813456b482c7a04b5db271f37226f7514d2ab006
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2020-05-03 14:35:38 +0300

    Added basic cocktails adapter that implements some common function to reduce boilerplate code using identical RecyclerView item in different fragments

---
+++ app/src/main/java/com/test/thecocktaildb/utils/recyclerViewAdapters/BaseCocktailsAdapter.kt
@@ -0,0 +1,19 @@
+package com.test.thecocktaildb.utils.recyclerViewAdapters
+
+import com.test.thecocktaildb.R
+import com.test.thecocktaildb.data.Cocktail
+import com.test.thecocktaildb.utils.recyclerViewAdapters.BaseAdapter
+
+abstract class BaseCocktailsAdapter<T> : BaseAdapter<Cocktail>() {
+
+    abstract fun setData(items: List<Cocktail>)
+
+    protected var cocktailsList: List<Cocktail> = emptyList()
+
+    override fun getItemForPosition(position: Int): Cocktail = cocktailsList[position]
+
+    override fun getLayoutIdForPosition(position: Int): Int = R.layout.cocktail_item
+
+    override fun getItemCount(): Int = cocktailsList.size
+
+}
