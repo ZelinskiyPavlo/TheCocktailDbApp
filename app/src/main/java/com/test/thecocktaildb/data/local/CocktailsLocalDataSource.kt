ccacfc1449682c14ec4eae80237359e2ddedeafd
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2020-05-03 22:24:40 +0300

    Added LocalDataSource that expose Room Dao to AppRepository

---
+++ app/src/main/java/com/test/thecocktaildb/data/local/CocktailsLocalDataSource.kt
@@ -0,0 +1,16 @@
+package com.test.thecocktaildb.data.local
+
+import com.test.thecocktaildb.data.Cocktail
+import io.reactivex.Completable
+import io.reactivex.Maybe
+import javax.inject.Inject
+
+class CocktailsLocalDataSource @Inject constructor(private val cocktailsDao: CocktailsDao) {
+
+    fun getCocktail(cocktailId: String): Maybe<Cocktail> =
+        cocktailsDao.getCocktail(cocktailId)
+
+    fun saveCocktail(cocktail: Cocktail): Completable =
+        cocktailsDao.saveCocktail(cocktail)
+
+}
