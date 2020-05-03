eff1aaa831cfdc4c05cba848acc11a97c51888c1
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2020-05-03 22:32:33 +0300

    Room interactions functions added to CocktailsRepository

--- app/src/main/java/com/test/thecocktaildb/data/CocktailsRepository.kt
+++ app/src/main/java/com/test/thecocktaildb/data/CocktailsRepository.kt
@@ -1,8 +1,14 @@
 package com.test.thecocktaildb.data
 
+import io.reactivex.Completable
+import io.reactivex.Maybe
 import io.reactivex.Single
 
 interface CocktailsRepository {
 
     fun searchCocktails(searchQuery: String): Single<Cocktails>
+
+    fun saveCocktails(cocktail: Cocktail): Completable
+
+    fun getCocktails(cocktailId: String): Maybe<Cocktail>
 }
