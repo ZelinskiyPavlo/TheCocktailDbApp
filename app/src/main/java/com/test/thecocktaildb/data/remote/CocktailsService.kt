05cad7918665ead82bc1cc10bf7ca6bd5e344857
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2020-05-03 14:06:34 +0300

    Added cocktails service which perform network api calls

---
+++ app/src/main/java/com/test/thecocktaildb/data/remote/CocktailsService.kt
@@ -0,0 +1,12 @@
+package com.test.thecocktaildb.data.remote
+
+import com.test.thecocktaildb.data.Cocktails
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CocktailsService {
+
+    @GET("search.php")
+    fun searchCocktails(@Query("s") searchQuery: String): Single<Cocktails>
+}
