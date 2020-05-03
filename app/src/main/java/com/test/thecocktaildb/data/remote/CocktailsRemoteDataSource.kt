df38003a7510fe5f8bf1eed1b614ee4a3c6c6797
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2020-05-03 14:14:00 +0300

    Added CocktailsRemoteDataSource layer for network data in CocktailsRepository

---
+++ app/src/main/java/com/test/thecocktaildb/data/remote/CocktailsRemoteDataSource.kt
@@ -0,0 +1,12 @@
+package com.test.thecocktaildb.data.remote
+
+import com.test.thecocktaildb.data.Cocktails
+import io.reactivex.Single
+import javax.inject.Inject
+
+class CocktailsRemoteDataSource @Inject constructor(
+    private val cocktailsService: CocktailsService) {
+
+    fun searchCocktails(searchQuery: String): Single<Cocktails> =
+        cocktailsService.searchCocktails(searchQuery)
+}
