7a116d9cabab5ba1ec8733ecfbf5bf5626189351
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2020-05-03 14:23:51 +0300

    Added basic fragment setting up functions for ViewModel and Data Binding

--- app/src/main/java/com/test/thecocktaildb/cocktailDetailsScreen/CocktailDetailsFragment.kt
+++ app/src/main/java/com/test/thecocktaildb/cocktailDetailsScreen/CocktailDetailsFragment.kt
@@ -1,7 +1,52 @@
 package com.test.thecocktaildb.cocktailDetailsScreen
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.test.thecocktaildb.R
+import com.test.thecocktaildb.databinding.CocktailDetailsFragmentBinding
+import com.test.thecocktaildb.databinding.SearchCocktailsFragmentBinding
 import com.test.thecocktaildb.di.Injectable
+import com.test.thecocktaildb.searchCocktailsScreen.SearchCocktailsViewModel
+import javax.inject.Inject
 
 class CocktailDetailsFragment: Injectable, Fragment() {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private lateinit var mViewModel: CocktailDetailsViewModel
+
+    private lateinit var viewDataBinding: CocktailDetailsFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        setupViewModel()
+
+        setupDataBinding(inflater, container)
+
+        return viewDataBinding.root
+    }
+
+    private fun setupViewModel() {
+        mViewModel =
+            ViewModelProvider(this, viewModelFactory)[CocktailDetailsViewModel::class.java]
+    }
+
+    private fun setupDataBinding(inflater: LayoutInflater, container: ViewGroup?){
+        viewDataBinding = CocktailDetailsFragmentBinding.inflate(inflater, container, false)
+            .apply {
+
+            }
+        viewDataBinding.lifecycleOwner = this.viewLifecycleOwner
+    }
+
 }
