76cc4862e402a999ae9c04a03a7b368f7cb4f928
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2020-05-04 15:04:12 +0300

    Added null check in Binding Adapter. Also added binding adapter for Ingredients adapter, bc it extends BaseAdapter not BaseCocktailAdapter

--- app/src/main/java/com/test/thecocktaildb/utils/BindingAdapters.kt
+++ app/src/main/java/com/test/thecocktaildb/utils/BindingAdapters.kt
@@ -5,6 +5,9 @@ import androidx.databinding.BindingAdapter
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.test.thecocktaildb.cocktailDetailsScreen.CocktailDetailsViewModel
+import com.test.thecocktaildb.cocktailDetailsScreen.Ingredient
+import com.test.thecocktaildb.cocktailDetailsScreen.IngredientsAdapter
 import com.test.thecocktaildb.cocktailsScreen.CocktailsAdapter
 import com.test.thecocktaildb.cocktailsScreen.CocktailsViewModel
 import com.test.thecocktaildb.data.Cocktail
@@ -17,7 +20,7 @@ fun /*ImageView.*/loadImage(imageView: ImageView, url: String?) =
     url?.let { Glide.with(imageView.context).load(url).into(imageView) }
 
 @BindingAdapter("app:items", "app:adapter_tag")
-fun /*RecyclerView.*/setItems(recyclerView: RecyclerView, items: List<Cocktail>, tag: ViewModel) {
+fun /*RecyclerView.*/setItems(recyclerView: RecyclerView, items: List<Cocktail>?, tag: ViewModel) {
 
     val recyclerViewAdapter = when (tag) {
         is CocktailsViewModel -> recyclerView.adapter as CocktailsAdapter
@@ -28,5 +31,14 @@ fun /*RecyclerView.*/setItems(recyclerView: RecyclerView, items: List<Cocktail>,
     }
 
     Timber.d("SetData Binding adapter setItems called")
-    recyclerViewAdapter.setData(items)
+    recyclerViewAdapter.setData(items ?: emptyList())
+}
+
+// Temporary solution (I guess)
+@BindingAdapter("app:ingredients")
+fun setIngredients(recyclerView: RecyclerView, items: List<Ingredient>?) {
+    with(recyclerView.adapter as IngredientsAdapter){
+        Timber.d("SetIngredients called")
+        setData(items ?: emptyList())
+    }
 }
