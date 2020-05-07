a5d9c126bd5ade22f07ae591e74d41e063a62644
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2020-05-07 16:12:34 +0300

    BindingAdapters extracted to extension functions to hide it from global namespace of autocompletion. Other code refactoring, removed unnecessary code

--- app/src/main/java/com/test/thecocktaildb/utils/BindingAdapters.kt
+++ app/src/main/java/com/test/thecocktaildb/utils/BindingAdapters.kt
@@ -5,7 +5,6 @@ import androidx.databinding.BindingAdapter
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.test.thecocktaildb.cocktailDetailsScreen.CocktailDetailsViewModel
 import com.test.thecocktaildb.cocktailDetailsScreen.Ingredient
 import com.test.thecocktaildb.cocktailDetailsScreen.IngredientsAdapter
 import com.test.thecocktaildb.cocktailsScreen.CocktailsAdapter
@@ -13,32 +12,25 @@ import com.test.thecocktaildb.cocktailsScreen.CocktailsViewModel
 import com.test.thecocktaildb.data.Cocktail
 import com.test.thecocktaildb.searchCocktailsScreen.SearchCocktailsAdapter
 import com.test.thecocktaildb.searchCocktailsScreen.SearchCocktailsViewModel
-import timber.log.Timber
 
 @BindingAdapter("app:image_url")
-fun /*ImageView.*/loadImage(imageView: ImageView, url: String?) =
-    url?.let { Glide.with(imageView.context).load(url).into(imageView) }
+fun ImageView.loadImage(url: String?) =
+    url?.let { Glide.with(this.context).load(url).into(this) }
 
 @BindingAdapter("app:items", "app:adapter_tag")
-fun /*RecyclerView.*/setItems(recyclerView: RecyclerView, items: List<Cocktail>?, tag: ViewModel) {
-
+fun RecyclerView.setItems(items: List<Cocktail>?, tag: ViewModel) {
     val recyclerViewAdapter = when (tag) {
-        is CocktailsViewModel -> recyclerView.adapter as CocktailsAdapter
-//        is CocktailsViewModel -> adapter as CocktailsAdapter
-        is SearchCocktailsViewModel -> recyclerView.adapter as SearchCocktailsAdapter
-//        is SearchCocktailsViewModel -> adapter as SearchCocktailsAdapter
+        is CocktailsViewModel -> adapter as CocktailsAdapter
+        is SearchCocktailsViewModel -> adapter as SearchCocktailsAdapter
         else -> throw ClassCastException()
     }
 
-    Timber.d("SetData Binding adapter setItems called")
     recyclerViewAdapter.setData(items ?: emptyList())
 }
 
-// Temporary solution (I guess)
 @BindingAdapter("app:ingredients")
-fun setIngredients(recyclerView: RecyclerView, items: List<Ingredient>?) {
-    with(recyclerView.adapter as IngredientsAdapter) {
-        Timber.d("SetIngredients called")
+fun RecyclerView.setIngredients(items: List<Ingredient>?) {
+    with(adapter as IngredientsAdapter) {
         setData(items ?: emptyList())
     }
 }
