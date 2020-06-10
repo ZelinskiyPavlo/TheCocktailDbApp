cd4b730b9a9652444c9373fa2aad3838d12cefb1
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2020-06-10 22:50:13 +0300

    Add toast when enabling airplane mode

---
+++ app/src/main/java/com/test/thecocktaildb/util/AirplaneReceiver.kt
@@ -0,0 +1,19 @@
+package com.test.thecocktaildb.util
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import timber.log.Timber
+
+class AirplaneReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, airplaneModeIntent: Intent?) {
+        Timber.i("onReceive method of AirplaneReceiver")
+        val airplaneModeStatus = airplaneModeIntent?.getBooleanExtra("state", false) ?: false
+        if (airplaneModeStatus){
+            val message = "Смакуйте маргаритку із відчуттям міри. І надіємося, що Ви не пілот під" +
+                    "  час польоту"
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
