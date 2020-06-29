d1090f4e652913b116e7a5e176a1a6a10f0c73ed
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2020-06-29 21:58:57 +0300

    Add popup menu on long click at cocktail item

--- app/src/main/java/com/test/thecocktaildb/ui/cocktailDetailsScreen/IngredientsAdapter.kt
+++ app/src/main/java/com/test/thecocktaildb/ui/cocktailDetailsScreen/IngredientsAdapter.kt
@@ -1,6 +1,8 @@
 package com.test.thecocktaildb.ui.cocktailDetailsScreen
 
 import com.test.thecocktaildb.R
+import com.test.thecocktaildb.data.Cocktail
+import com.test.thecocktaildb.util.CocktailsItemUserActionListener
 import com.test.thecocktaildb.util.CustomActionListener
 import com.test.thecocktaildb.util.recyclerViewAdapters.BaseAdapter
 
@@ -22,7 +24,16 @@ class IngredientsAdapter :
     override fun getItemCount(): Int = ingredientsList.size
 
     override fun getItemClickListener(): CustomActionListener {
-        return object : CustomActionListener {}
+        return object : CocktailsItemUserActionListener {
+            override fun onFavoriteIconClicked(cocktail: Cocktail) {
+            }
+
+            override fun onItemClicked(cocktail: Cocktail) {}
+
+            override fun onItemLongClicked(view: View, cocktail: Cocktail): Boolean {
+                return true
+            }
+        }
     }
 
 
