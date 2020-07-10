30bc7b00d63f446322a1944c163653b55fcd28d9
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2020-07-10 14:52:50 +0300

    Add more options to ImageView binding adapter

--- app/src/main/java/com/test/thecocktaildb/dataBinding/adapter/ImageViewBindingAdapter.kt
+++ app/src/main/java/com/test/thecocktaildb/dataBinding/adapter/ImageViewBindingAdapter.kt
@@ -1,16 +1,37 @@
 package com.test.thecocktaildb.dataBinding.adapter
 
 import android.content.res.ColorStateList
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import androidx.core.widget.ImageViewCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.test.thecocktaildb.R
 
-@BindingAdapter("bind:iv_imageUrl")
-fun ImageView.loadImage(url: String?) =
-    url?.let { Glide.with(this.context).load(url).into(this) }
+@BindingAdapter(
+    value = ["bind:iv_imageUrl", "bind:placeholderUrl", "bind:placeholderResId",
+        "bind:errorUrl", "bind:errorResId", "bind:isCrossFadeEnabled"], requireAll = false
+)
+fun ImageView.loadImage(
+    url: String?, placeholderUrl: Drawable?, placeholderResId: Int?, errorUrl: Drawable?,
+    errorResId: Int?, isCrossFadeEnabled: Boolean?
+) {
+    url?.let {_ ->
+        val imageBuilder = Glide.with(this.context).load(url)
+
+        placeholderUrl?.let { imageBuilder.placeholder(it) }
+        placeholderResId?.let { imageBuilder.placeholder(it) }
+
+        errorUrl?.let { imageBuilder.error(it) }
+        errorResId?.let { imageBuilder.error(it) }
+
+        if (isCrossFadeEnabled == true) imageBuilder.transition(DrawableTransitionOptions.withCrossFade())
+
+        imageBuilder.into(this)
+    }
+}
 
 @BindingAdapter("bind:iv_batteryBar_tint")
 fun ImageView.setTint(isCharging: Boolean) {
