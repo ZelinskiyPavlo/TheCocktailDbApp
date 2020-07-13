f7e96e13db05d0323436a8e6e4ceb9b440adafa5
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2020-07-13 15:02:01 +0300

    Fix AuthActivity

--- app/src/main/java/com/test/thecocktaildb/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/test/thecocktaildb/ui/auth/AuthActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.text.InputFilter
 import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.test.thecocktaildb.R
 import com.test.thecocktaildb.databinding.ActivityAuthBinding
 import com.test.thecocktaildb.ui.base.BaseActivity
@@ -16,21 +18,35 @@ import kotlinx.android.synthetic.main.activity_auth.*
 
 class AuthActivity : BaseActivity() {
 
-    val viewModel: AuthViewModel by viewModels()
+    private val viewModel: AuthViewModel by viewModels()
+
+    private lateinit var dataBinding: ActivityAuthBinding
+
+    private val login = "SomeLogin"
+    private val password = "123456a"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-
-//        TODO: implement in baseActivity (in branch_7)
-        DataBindingUtil.setContentView<ActivityAuthBinding>(this, R.layout.activity_auth).also {
-            it.lifecycleOwner = this
-            it.viewModel = this.viewModel
-            it.activity = this
-        }
+        setupDataBinding()
+        setupObserver()
 
         setWhiteSpaceFilter()
         viewModel.setInitialText()
+    }
+
+    private fun setupDataBinding() {
+        dataBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_auth)
+        dataBinding.lifecycleOwner = this
+        dataBinding.viewModel = viewModel
+        dataBinding.activity = this
+    }
+
+    private fun setupObserver() {
+        viewModel.clearErrorTextColorEventLiveData.observe(this, Observer {
+            changeEditTextColorToRed(false)
+        })
     }
 
     private fun setWhiteSpaceFilter() {
@@ -44,12 +60,25 @@ class AuthActivity : BaseActivity() {
 
     fun onLoginButtonClicked() {
         val view = this.currentFocus
+        when {
+            viewModel.errorLoginTextColorLiveData.value == true -> {
+                changeEditTextColorToRed(true)
+                dataBinding.loginEditText.requestFocus()
+                return
+            }
+            viewModel.errorPasswordTextColorLiveData.value == true -> {
+                changeEditTextColorToRed(true)
+                dataBinding.passwordEditText.requestFocus()
+                return
+            }
+        }
         view?.let { v ->
             val imm =
                 this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(v.windowToken, 0)
         }
-        if (viewModel.isLoginDataValidLiveData.value == true) {
+        if (viewModel.loginInputLiveData.value == login
+            && viewModel.passwordInputLiveData.value == password) {
             val intent = Intent(this, CocktailsActivity::class.java)
             this.startActivity(intent)
             finish()
@@ -64,5 +93,19 @@ class AuthActivity : BaseActivity() {
             rightButtonText = "Ok"
             descriptionText = "Looks like you provided wrong login or password"
         }.show(supportFragmentManager, "SignInErrorDialog")
+    }
+
+    private fun changeEditTextColorToRed(isColorRed: Boolean) {
+        val errorTextColor = ContextCompat.getColor(this, R.color.error_text)
+        val regularTextColor = ContextCompat.getColor(this, R.color.text_default)
+        if (isColorRed) {
+            if (viewModel.errorLoginTextColorLiveData.value == true)
+                dataBinding.loginEditText.setTextColor(errorTextColor)
+            if (viewModel.errorPasswordTextColorLiveData.value == true)
+                dataBinding.passwordEditText.setTextColor(errorTextColor)
+        } else {
+            dataBinding.loginEditText.setTextColor(regularTextColor)
+            dataBinding.passwordEditText.setTextColor(regularTextColor)
+        }
     }
 }
