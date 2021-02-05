aab8d2e03b0ef9b2546e26f269d8cb489ee27446
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2021-02-06 00:15:16 +0200

    Add function to turn on dagger-reflect when dagger.reflect prop is true
    Note: currently not working! Dagger-reflect replace kapt for reflection
    in debug build for faster build time. But it has some problems with
    nested @ContributeAndroidInjector annotations so i try to use one branch
    from forked repo which should work, but i get unauthorized access error
    in jitpack. When this bug will be resolved, I will have ability to
    instantly turn on reflection if I use this function instead of manually
    specifying kapt. To turn on/off reflect you need to change property
    dagger.reflect to true or false in gradle.properties.

--- buildSrc/src/main/kotlin/com/test/gradle/dependency/DaggerDependencies.kt
+++ buildSrc/src/main/kotlin/com/test/gradle/dependency/DaggerDependencies.kt
@@ -1,18 +1,15 @@
 package com.test.gradle.dependency
 
+import Lib
 import org.gradle.kotlin.dsl.DependencyHandlerScope
 
-fun DependencyHandlerScope.daggerDependencies() {
-
-    "implementation"(Lib.Dagger.dagger)
-    "kapt"(Lib.Dagger.daggerCompiler)
-}
-
-fun DependencyHandlerScope.daggerAndroidDependencies() {
-
-    "implementation"(Lib.Dagger.DaggerAndroid.daggerAndroidSupport)
-    "implementation"(Lib.Dagger.DaggerAndroid.daggerAndroid)
-    "kapt"(Lib.Dagger.DaggerAndroid.daggerAndroidProcessor)
-
-    "implementation"(Lib.Dagger.ComponentManager.componentManager)
+fun DependencyHandlerScope.daggerKaptDependencies(daggerReflectProperty: Any?) {
+    if (daggerReflectProperty == "true") {
+        "lintChecks"("com.github.kokeroulis:dagger-reflect-lint:atsiap~module_with_generic_try_to_add_workaround_new_fixes-SNAPSHOT:0.1.0")
+        "kapt"("com.github.kokeroulis:dagger-reflect-compiler:atsiap~module_with_generic_try_to_add_workaround_new_fixes-SNAPSHOT:0.1.0")
+        "implementation"("com.github.kokeroulis:dagger-reflect:atsiap~module_with_generic_try_to_add_workaround_new_fixes-SNAPSHOT:0.1.0")
+    } else {
+        "kapt"(Lib.Dagger.daggerCompiler)
+        "kapt"(Lib.Dagger.DaggerAndroid.daggerAndroidProcessor)
+    }
 }
