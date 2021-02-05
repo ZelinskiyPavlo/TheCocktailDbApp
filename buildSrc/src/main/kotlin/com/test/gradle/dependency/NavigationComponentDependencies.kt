3809fa24148bbf4a4b1083d873c179318006a196
Pavlo <zelinskiypavlo@gmail.com>
2021-02-06 00:29:23 +0200

    Remove old gradle kts extension

--- buildSrc/src/main/kotlin/com/test/gradle/dependency/NavigationComponentDependencies.kt
+++
@@ -1,9 +0,0 @@
-package com.test.gradle.dependency
-
-import org.gradle.kotlin.dsl.DependencyHandlerScope
-
-fun DependencyHandlerScope.navigationComponentDependencies() {
-
-    "implementation"(Lib.AndroidX.Navigation.fragmentKtx)
-    "implementation"(Lib.AndroidX.Navigation.uiKtx)
-}
--- buildSrc/src/main/kotlin/com/test/gradle/dependency/typical/DefaultDependencies.kt
+++
@@ -1,8 +0,0 @@
-package com.test.gradle.dependency.typical
-
-import org.gradle.kotlin.dsl.DependencyHandlerScope
-
-fun DependencyHandlerScope.defaultDependencies() {
-
-    "implementation"(Lib.Logging.timber)
-}
