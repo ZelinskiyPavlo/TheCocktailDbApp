86a952c48d73eac2fd25d1ec1867fa622e13b51f
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2021-02-06 00:04:02 +0200

    Add dagger modules for activity and fragments sub components

--- app/src/main/java/com/test/thecocktaildb/di/module/FragmentModule.kt
+++ app/src/main/java/com/test/thecocktaildb/di/module/FragmentModule.kt
@@ -1,83 +1,25 @@
 package com.test.thecocktaildb.di.module
 
-import com.test.thecocktaildb.presentation.ui.auth.AuthActivity
-import com.test.thecocktaildb.presentation.ui.auth.login.LoginFragment
-import com.test.thecocktaildb.presentation.ui.auth.register.RegisterFragment
-import com.test.thecocktaildb.presentation.ui.auth.splash.SplashFragment
-import com.test.thecocktaildb.presentation.ui.cocktail.MainActivity
-import com.test.thecocktaildb.presentation.ui.cocktail.favorite.FavoriteFragment
-import com.test.thecocktaildb.presentation.ui.cocktail.filter.FilterFragment
-import com.test.thecocktaildb.presentation.ui.cocktail.history.HistoryFragment
-import com.test.thecocktaildb.presentation.ui.cocktail.host.HostFragment
-import com.test.thecocktaildb.presentation.ui.detail.CocktailDetailsFragment
-import com.test.thecocktaildb.presentation.ui.search.SearchCocktailFragment
-import com.test.thecocktaildb.presentation.ui.setting.SettingFragment
-import com.test.thecocktaildb.presentation.ui.setting.cube.CubeFragment
-import com.test.thecocktaildb.presentation.ui.setting.profile.ProfileFragment
-import com.test.thecocktaildb.presentation.ui.setting.rangeseekbar.RangeSeekBarFragment
+import com.test.auth.di.AuthFragmentModule
+import com.test.auth.di.navigation.AuthCiceroneModule
+import com.test.auth.ui.AuthHostFragment
+import com.test.dagger.scope.PerFragment
+import com.test.splash.ui.SplashFragment
+import com.test.thecocktaildb.di.module.navigation.AuthNavigationModule
+import com.test.thecocktaildb.di.module.navigation.SplashNavigationModule
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
-abstract class FragmentModule {
+interface FragmentModule {
 
-    @ContributesAndroidInjector
     @Suppress("unused")
-    abstract fun contributeCocktailsFragment(): HistoryFragment
+    @PerFragment
+    @ContributesAndroidInjector(modules = [SplashNavigationModule::class])
+    fun contributeSplashFragment(): SplashFragment
 
-    @ContributesAndroidInjector
     @Suppress("unused")
-    abstract fun contributeCocktailDetailsFragment(): CocktailDetailsFragment
-
-    @ContributesAndroidInjector
-    @Suppress("unused")
-    abstract fun contributeSearchCocktailsFragment(): SearchCocktailFragment
-
-    @ContributesAndroidInjector
-    @Suppress("unused")
-    abstract fun contributeFilterFragment(): FilterFragment
-
-    @ContributesAndroidInjector
-    @Suppress("unused")
-    abstract fun contributeHostFragment(): HostFragment
-
-    @ContributesAndroidInjector
-    @Suppress("unused")
-    abstract fun contributeFavoriteFragment(): FavoriteFragment
-
-    @ContributesAndroidInjector
-    @Suppress("unused")
-    abstract fun contributeSettingFragment(): SettingFragment
-
-    @ContributesAndroidInjector
-    @Suppress("unused")
-    abstract fun contributeProfileFragment(): ProfileFragment
-
-    @ContributesAndroidInjector
-    @Suppress("unused")
-    abstract fun contributeMainActivity(): MainActivity
-
-    @ContributesAndroidInjector
-    @Suppress("unused")
-    abstract fun contributeAuthActivity(): AuthActivity
-
-    @ContributesAndroidInjector
-    @Suppress("unused")
-    abstract fun contributeSplashFragment(): SplashFragment
-
-    @ContributesAndroidInjector
-    @Suppress("unused")
-    abstract fun contributeLoginFragment(): LoginFragment
-
-    @ContributesAndroidInjector
-    @Suppress("unused")
-    abstract fun contributeRegisterFragment(): RegisterFragment
-
-    @ContributesAndroidInjector
-    @Suppress("unused")
-    abstract fun contributeCubeFragment(): CubeFragment
-
-    @ContributesAndroidInjector
-    @Suppress("unused")
-    abstract fun contributeRangeSeekBarFragment(): RangeSeekBarFragment
+    @PerFragment
+    @ContributesAndroidInjector(modules = [AuthFragmentModule::class, AuthCiceroneModule::class, AuthNavigationModule::class])
+    fun contributeAuthHostFragment(): AuthHostFragment
 }
--- core/dagger/src/main/java/com/test/dagger/scope/PerFragment.kt
+++ core/dagger/src/main/java/com/test/dagger/scope/PerFragment.kt
@@ -5,4 +5,4 @@ import javax.inject.Scope
 @Scope
 @MustBeDocumented
 @kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
-annotation class PerScreen
+annotation class PerFragment
