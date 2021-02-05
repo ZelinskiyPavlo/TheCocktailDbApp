62401779b4b6775b02f441bfc92ff1b836968a25
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2021-02-05 23:48:31 +0200

    Add Auth Fragment which serves as a container for auth related screens
    It implement internal cicerone navigation

---
+++ feature/auth/src/main/java/com/test/auth/navigation/routing/Screen.kt
@@ -0,0 +1,12 @@
+package com.test.auth.navigation.routing
+
+import com.github.terrakok.cicerone.androidx.FragmentScreen
+import com.test.login.ui.LoginFragment
+import com.test.register.ui.RegisterFragment
+
+object Screen {
+
+    fun login() = FragmentScreen("LOGIN") { LoginFragment.newInstance() }
+
+    fun register() = FragmentScreen("REGISTER") { RegisterFragment.newInstance() }
+}
