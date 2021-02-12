ab37447b1b3a9fd2727e1bae76004c8a2cddf685
ZelinskiyPavlo <zelinskiypavlo@gmail.com>
2021-02-12 10:00:43 +0200

    Add Simple Navigator which add ability to simply exit current screen
    It can be used in situation when you can only exit from current screen,
    but you need to do it by pressing back button for example. In situation
    when you can only exit using back button this navigator isn't necessary.

---
+++ app/src/main/java/com/test/thecocktaildb/di/module/navigation/SimpleNavigationModule.kt
@@ -0,0 +1,13 @@
+package com.test.thecocktaildb.di.module.navigation
+
+import com.test.navigation.api.SimpleNavigatorApi
+import com.test.thecocktaildb.navigation.SimpleNavigatorImpl
+import dagger.Binds
+import dagger.Module
+
+@Module
+interface SimpleNavigationModule {
+
+    @Binds
+    fun bindSimpleNavigator(simpleNavigatorImpl: SimpleNavigatorImpl): SimpleNavigatorApi
+}
